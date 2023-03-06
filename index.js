2a177892832874f9a1c53af7f9f5f233d07c5e4b
Kevin-Zapata <kevin19981921@gmail.com>
2023-03-06 18:45:27 -0500

    Aprendiendosobre GET:recibir parametros

--- index.js
+++ index.js
@@ -8,11 +8,31 @@ app.get('/',(req, res) =>{
 app.get('/nueva-ruta',(req, res) =>{
   res.send('Hola soy una nueva ruta')
 })
-app.get('/productos',(req, res) =>{
-  res.json({
-    name:'Producto 1',
+app.get('/products',(req, res) =>{
+  res.json([
+    {name:'Product 1',
     price: 1000
+  },
+  {name:'Product 2',
+    price: 2000
+  }
+  ])
 })
+app.get('/products/:id', (req, res)=>{
+  const {id}= req.params; // Aqui buscamos identificar el id que viene desde el navegador, en este caso se lo asignamos,aqui recibimos un parametro
+  res.json({
+    id,
+    name:'Product 2',
+    price: 2000
+})
+})
+app.get('/categories/:categoryId/products/:productsId', (req, res)=>{ // en esta parte recibimos 2 parametros en una misma url
+  const { categoryId, productsId } = req.params;
+  res.json({
+    categoryId,
+    productsId
+  })
+
 })
 app.listen(port, ()=>{
   console.log('Mi port '+ port)
