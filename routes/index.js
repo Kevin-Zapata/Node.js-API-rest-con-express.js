6c3647751bd6c2f32c9cb9f1293e0dca02adff85
Kevin-Zapata <kevin19981921@gmail.com>
2023-03-14 08:00:51 -0500

    Aprendiendo POST

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+const express = require('express');
 const productsRouter = require('./productsRouter')
 const userRouter = require('./userRouter')
 function routerApi(app){
-app.use('/user',userRouter);
-app.use('/products',productsRouter); //Tener en cuenta que aqui no llamamos archivos ponemos la ruta como tal, no un archivo.
+  const router = express.Router();
+  app.use('/api/v1',router);
+router.use('/user',userRouter);
+router.use('/products',productsRouter); //Tener en cuenta que aqui no llamamos archivos ponemos la ruta como tal, no un archivo.
 }
 module.exports = routerApi;
