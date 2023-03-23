55215d8457e9a94191442cb6609c52624a0b8f13
Kevin-Zapata <kevin19981921@gmail.com>
2023-03-22 23:47:53 -0500

    Aprendiendo PUT, PATCH y codigos de estado

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -32,21 +32,55 @@ router.get ('/filter', (req,res)=>{ //Aquio con el /filter puesto de esta manera
   res.send('Yo soy un filter');
 });
 // Tener en cuenta si tengo una ruta /products/<cualquiercosa> y antes de eso tengo un /products/:id lo tomara como parametro debe colocar el :id despues del de la ruta especifica para que no pase el error.
-router.get('/:id', (req, res)=>{
-  const {id}= req.params; // Aqui buscamos identificar el id que viene desde el navegador, en este caso se lo asignamos,aqui recibimos un parametro
-  res.json({
-    id,
-    name:'Product 2',
-    price: 2000
+router.get('/:id', (req, res)=>{// Todos los parametros recibidos aqui pasan hacer un string
+  const {id}= req.params; // Aqui busca mos identificar el id que viene desde el navegador, en este caso se lo asignamos,aqui recibimos un parametro
+  if (id === '999'){ //Tener en cuenta los ''
+  res.status(404).json({
+   message:"not found"
 });
+} else {
+  res.status(200).json({
+  id,
+  name:'Product 2',
+  price: 2000
+  });
+  }
 });
 
 router.get('/categories/:categoryId/products/:productsId', (req, res)=>{ // en esta parte recibimos 2 parametros en una misma url
   const { categoryId, productsId } = req.params;
+
+    res.json({
+      message:"not found"
+    })
+
   res.json({
     categoryId,
     productsId
-  })
+  });
 
+});
+router.post('/', (req, res)=>{
+  const body = req.body
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+})
+router.patch('/:id', (req, res)=>{
+  const{ id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'updated',
+    data: body,
+    id
+  });
+})
+router.delete('/:id', (req, res)=>{
+  const{ id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
 })
 module.exports = router;
