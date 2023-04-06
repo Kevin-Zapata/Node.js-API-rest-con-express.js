0af0d7a74252e906c8ee9a17a9bfea669ee4b72c
Kevin-Zapata <kevin19981921@gmail.com>
2023-04-05 19:39:59 -0500

    Aprendiendo sore middlewares

--- Services/products.service.js
+++ Services/products.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 class ProductsService {
 constructor(){ //Esto es un array en memoria
   this.products =[];
@@ -34,12 +35,13 @@ async create(data){
 
 }
 async findOne(id){
-return this.products.find(item =>item.id === id )
+const product = this.products.find(item =>item.id === id )
+if(!product)
 }
 async update(id, changes){
 const index = this.products.findIndex(item =>item.id === id ) //Obtiene la posicion dentro del array
 if(index === -1){
-  throw new Error('Product not found');
+  throw boom.notFound('Product not found');
 }
 const product = this.products[index];
 this.products[index] ={
@@ -52,7 +54,7 @@ return this.products[index];
 async deletee(id){
   const index = this.products.findIndex(item =>item.id === id ); //Obtiene la posicion dentro del array
   if(index === -1){ // es menos -1 porque cuando no encuentra dicho ID genera un -1 indicando que es un error
-    throw new Error('Product not found')
+    throw boom.notFound('Product not found');
   }
   this.products.splice(index, 1);
   return{ id };
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+const {logErrors, errorHandler} = require('./middlewares/error.handler')
+
 app.use(express.json());
 
 app.get('/',(req, res) =>{
@@ -18,3 +20,6 @@ app.listen(port, ()=>{
   console.log('Mi port '+ port)
 });
 routerApi(app);
+ //Tener en cuenta al momento de colocar los middleware, se deben colocar despues del router
+app.use (logErrors); //Tambien el orden en que se ejecutan
+app.use(errorHandler);
